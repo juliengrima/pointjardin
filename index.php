5a15e02e8a5ce059543ad9b80b4393017117036b
julien_grima <gogrinformatique@gmail.com>
2016-09-09 10:29:26 +0200

    - modification des images en liens 	- ajout d'une ligne de tuiles

--- index.php
+++ index.php
@@ -25,37 +25,71 @@ include('includes/header.php');
 </div>
 </div>
       
-      
+<!-- Ligne de tuiles lien vers pages 1 -->      
 <div class="container">
-            <div class="row">
-                <div class="col s12 m4">
-                    <div class="zoomarticle2">
-                        <div class="article_big_img2">
-                            <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
-                        </div>
-                    </div>
+    <div class="row">
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
                 </div>
-                <!--<div class="row">-->
-                    <div class="col s12 m4">
-                        <div class="zoomarticle2">
-                            <div class="article_big_img2">
-                                <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
-                            </div>
-                        </div>
-                  </div>
-                    <!--<div class="row">-->
-                        <div class="col s12 m4">
-                            <div class="zoomarticle2">
-                                <div class="article_big_img2">
-                                    <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
-                                </div>
-                            </div>
-                       </div>
-                    <!--</div>-->
-                <!--</div>-->
-            </div>
+            </a>
         </div>
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
+                </div>
+             </a>
+        </div>
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
+                </div>
+             </a>
+        </div>
+    </div>
+</div>
 
-    
+<!-- Ligne de tuiles lien vers pages 2 -->
+<div class="container">
+    <div class="row">
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
+                </div>
+            </a>
+        </div>
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
+                </div>
+             </a>
+        </div>
+        <div class="col s12 m4">
+            <a href="">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/a1.jpg" alt="a1.jpg" height=100% width=100%>
+                    </div>   
+                </div>
+             </a>
+        </div>
+    </div>
+</div>
+
 
 <?php include('includes/footer.php'); ?>
