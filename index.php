46021daa1b5c0f36230ee07256e5cf1c5d277d08
julien_grima <gogrinformatique@gmail.com>
2016-09-15 13:40:24 +0200

    retablissement images

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('includes/header.php');
         <!--CORPS DE LA PAGE-->
        
 <!-- REPERES DE COLONNES POUR IMPLEMENTATION -->
-<div class="container center">
+<!-- <div class="container center">
 <div class="row">
 <div class="col s1">1</div>
 <div class="col s1">2</div>
@@ -23,27 +23,38 @@ include('includes/header.php');
 <div class="col s1">11</div>
 <div class="col s1">12</div>
 </div>
-</div>
+</div>-->
      
-<div class="container center">
-    <div class="row center">
+<div class="container">
+    <div class="row">
         <div class="col s12 m6 center">
-            <center>
-                <table>
-                   <tr>
-                       <td>
-                           Bonjour et bienvenue, <br>
-                           dans le monde du jardinage <br> professionnel et particulier.<br>
-                           Chez nous vous découvrirez <br> les dernières tendances,<br>
-                           les derniers outils et <br> surtout une large gamme de <br>
-                           produits
-                       </td>
-                   </tr>
-                </table> 
-            </center>  
+                <div class="welcome">
+                    <h5>BIENVENUE CHEZ POINT JARDIN !</h5>
+                    <p>Paul et Laurentine vous accueillent dans votre 
+                    nouveau point de vente de matériaux pour 
+                    l’aménagement du jardin.</p>
+                    <table>
+                        <tr>
+                            <td class="taille"><img class="icone" src="images/img01.png" /></td>
+                            <td>Achetez la quantité dont vous avez besoin</td>
+                        </tr>
+                        <tr>
+                            <td class="taille"><img class="icone" src="images/img02.png" /></td>
+                            <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
+                        </tr>
+                        <tr>
+                            <td class="taille"><img class="icone" src="images/img03.png" /></td>
+                            <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
+                        </tr>
+                        <tr>
+                            <td class="taille"><img class="icone" src="images/img04.png" /></td>
+                            <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
+                        </tr>
+                    </table>
+                </div> 
         </div>
         <div class="col s12 m6 center">
-            <video controls poster="images/logo.jpg" height=100% width=100% class="center">
+            <video controls poster="images/logo.jpg" height=100% width=100% class="center haut">
                 <source src="videos/present.mp4">
                 <source src="videos/present.mov">
                 <source src="videos/present.flv">
@@ -51,65 +62,61 @@ include('includes/header.php');
             </video>
         </div>
     </div>
-</div>
+
       
 <!-- Ligne de tuiles lien vers pages 1 -->      
-<div class="container">
+
     <div class="row">
-        <div class="col s12 m4">
+        <div class="col s12 m6 l4">
             <a href="">
                 <div class="zoomarticle2">
                     <div class="article_big_img2">
-                        <img src="images/galets.jpg" alt="Mineral" height=100% width=100%>
+                        <img src="images/galets.jpg" alt="Mineral">
                     </div>   
                 </div>
             </a>
         </div>
-        <div class="col s12 m4">
+        <div class="col s12 m6 l4">
             <a href="">
                 <div class="zoomarticle2">
                     <div class="article_big_img2">
-                        <img src="images/chanvre.jpg" alt="vegetal" height=100% width=100%>
+                        <img src="images/chanvre.jpg" alt="vegetal">
                     </div>   
                 </div>
              </a>
         </div>
-        <div class="col s12 m4">
+        <div class="col s12 m6 l4">
             <a href="">
                 <div class="zoomarticle2">
                     <div class="article_big_img2">
-                        <img src="images/gazon" alt="Accessoires" height=100% width=100%>
+                        <img src="images/gazon" alt="Accessoires">
                     </div>   
                 </div>
              </a>
         </div>
     </div>
-</div>
+
 
 <!-- Ligne de tuiles lien vers pages 2 -->
-<div class="container center">
+
     <div class="row">
-        <div class="col s12 m6 center">
-            <center>
+        <div class="col s12 m6 l6 center">
                 <a href="">
-                    <div class="zoomarticle1">
-                        <div class="article_big_img1">
-                            <img src="images/livraison" alt="Services" height=70% width=70%>
+                    <div class="zoomarticle2">
+                        <div class="article_big_img2">
+                            <img src="images/livraison" alt="Services" class="large">
                         </div>   
                     </div>
                 </a>
-            </center>
         </div>
-        <div class="col s12 m6">
-           <center>
-                <a href="pages/concept.php">
-                    <div class="zoomarticle1">
-                        <div class="article_big_img1">
-                            <img src="images/concept1" alt="Conceptions" height=70% width=70%>
-                        </div>   
-                    </div>
-                </a>
-            </center>
+        <div class="col s12 m6 l6 center">
+            <a href="pages/concept.php">
+                <div class="zoomarticle2">
+                    <div class="article_big_img2">
+                        <img src="images/concept1" alt="Conceptions" class="large center">
+                    </div>   
+                </div>
+            </a>
         </div>
     </div>
 </div>
