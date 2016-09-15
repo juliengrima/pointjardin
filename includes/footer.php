cfc0055cc2b07a7cbe3cae1ec79018108b95c95c
julien_grima <gogrinformatique@gmail.com>
2016-09-15 18:09:14 +0200

    Amelioration

--- includes/footer.php
+++ includes/footer.php
@@ -46,6 +46,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    
+    <SCRIPT>$(document).ready(function(){
+      $('.parallax').parallax();
+    });</SCRIPT>
 
     <!-- Appel du script facebook -->
     <div id="fb-root"></div>
--- index.php
+++ index.php
@@ -4,11 +4,9 @@ include('includes/header.php');
 //include('includes/body1.php');
 
 ?>
-        
-        <!--CORPS DE LA PAGE-->
-       
-<!-- REPERES DE COLONNES POUR IMPLEMENTATION -->
-<!-- <div class="container center">
+    <!--CORPS DE LA PAGE-->
+    <!-- REPERES DE COLONNES POUR IMPLEMENTATION -->
+    <!-- <div class="container center">
 <div class="row">
 <div class="col s1">1</div>
 <div class="col s1">2</div>
@@ -24,102 +22,138 @@ include('includes/header.php');
 <div class="col s1">12</div>
 </div>
 </div>-->
-     
-<div class="container">
-    <div class="row">
-        <div class="col s12 m6 center">
-                <div class="welcome">
-                    <h5>BIENVENUE CHEZ POINT JARDIN !</h5>
-                    <p>Paul et Laurentine vous accueillent dans votre 
-                    nouveau point de vente de matériaux pour 
-                    l’aménagement du jardin.</p>
-                    <table>
-                        <tr>
-                            <td class="taille"><img class="icone" src="images/img01.png" /></td>
-                            <td>Achetez la quantité dont vous avez besoin</td>
-                        </tr>
-                        <tr>
-                            <td class="taille"><img class="icone" src="images/img02.png" /></td>
-                            <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
-                        </tr>
-                        <tr>
-                            <td class="taille"><img class="icone" src="images/img03.png" /></td>
-                            <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
-                        </tr>
-                        <tr>
-                            <td class="taille"><img class="icone" src="images/img04.png" /></td>
-                            <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
-                        </tr>
-                    </table>
-                </div> 
+   
+    <!--<div class="parallax-container">
+        <div class="parallax">
+            <img src="images/secret_garden.jpg">
+            <div><p class="colorbis">TESTTESTESTEFSFEFF?SCKWCJ</p></div>
+            
         </div>
-        <div class="col s12 m6 center">
-            <video controls poster="images/logo.jpg" height=100% width=100% class="center haut">
-                <source src="videos/present.mp4">
-                <source src="videos/present.mov">
-                <source src="videos/present.flv">
-                <a href="https://www.youtube.com/watch?v=szGkJgvt2-0">Alternative de la vidéo :</a> 
-            </video>
-        </div>
-    </div>
-
-      
-<!-- Ligne de tuiles lien vers pages 1 -->      
-
-    <div class="row">
-        <div class="col s12 m6 l4">
-            <a href="">
-                <div class="zoomarticle2">
-                    <div class="article_big_img2">
-                        <img src="images/galets.jpg" alt="Mineral">
-                    </div>   
-                </div>
-            </a>
-        </div>
-        <div class="col s12 m6 l4">
-            <a href="">
-                <div class="zoomarticle2">
-                    <div class="article_big_img2">
-                        <img src="images/chanvre.jpg" alt="vegetal">
-                    </div>   
-                </div>
-             </a>
-        </div>
-        <div class="col s12 m6 l4">
-            <a href="">
-                <div class="zoomarticle2">
-                    <div class="article_big_img2">
-                        <img src="images/gazon" alt="Accessoires">
-                    </div>   
-                </div>
-             </a>
-        </div>
-    </div>
-
-
-<!-- Ligne de tuiles lien vers pages 2 -->
-
-    <div class="row">
-        <div class="col s12 m6 l6 center">
-                <a href="">
-                    <div class="zoomarticle2">
-                        <div class="article_big_img2">
-                            <img src="images/livraison" alt="Services" class="large">
-                        </div>   
+    </div>-->
+   
+           <div class="container"> 
+                <div class="row">
+                    <div class="col s12 m6 center">
+                        <div class="welcome">
+                            <h5>BIENVENUE CHEZ POINT JARDIN !</h5>
+                            <p>Paul et Laurentine vous accueillent dans votre nouveau point de vente de matériaux pour l’aménagement du jardin.</p>
+                            <table>
+                                <tr>
+                                    <td class="taille"><img class="icone" src="images/img01.png" /></td>
+                                    <td>Achetez la quantité dont vous avez besoin</td>
+                                </tr>
+                                <tr>
+                                    <td class="taille"><img class="icone" src="images/img02.png" /></td>
+                                    <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
+                                </tr>
+                                <tr>
+                                    <td class="taille"><img class="icone" src="images/img03.png" /></td>
+                                    <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
+                                </tr>
+                                <tr>
+                                    <td class="taille"><img class="icone" src="images/img04.png" /></td>
+                                    <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
-                </a>
-        </div>
-        <div class="col s12 m6 l6 center">
-            <a href="pages/concept.php">
-                <div class="zoomarticle2">
-                    <div class="article_big_img2">
-                        <img src="images/concept1" alt="Conceptions" class="large center">
-                    </div>   
+                    <div class="col s12 m6 center">
+                        <video controls poster="images/logo.jpg" height=100% width=100% class="responsive-video center haut">
+                            <source src="videos/present.mp4">
+                            <source src="videos/present.mov">
+                            <source src="videos/present.flv"> <a href="https://www.youtube.com/watch?v=szGkJgvt2-0">Alternative de la vidéo :</a> </video>
+                    </div>
                 </div>
-            </a>
+                <!-- Ligne de tuiles lien vers pages 1 -->
+                <div class="row">
+                    <div class="col s12 m6 l4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" alt="livraison services" src="images/galets.jpg">
+                            </div>
+                            <div class="card-content"> 
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal"> 
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" alt="livraison services" src="images/chanvre.jpg">
+                            </div>
+                            <div class="card-content"> 
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal"> 
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" alt="livraison services" src="images/gazon.jpg">
+                            </div>
+                            <div class="card-content"> 
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal"> 
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Ligne de tuiles lien vers pages 2 -->
+                <div class="row">
+                    <div class="col s12 m6 l6">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" alt="livraison services" src="images/livraison">
+                            </div>
+                            <div class="card-content"> 
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal"> 
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col s12 m6 l6 center">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" alt="livraison services" src="images/concept1">
+                            </div>
+                            <div class="card-content"> 
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal"> 
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+  
+    <!--<div class="parallax-container">
+        <div class="parallax">
+            <img src="images/secret_garden.jpg">
         </div>
-    </div>
-</div>
+    </div>-->
 
 
-<?php include('includes/footer.php'); ?>
+    <?php include('includes/footer.php'); ?>
