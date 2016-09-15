ada86c65e0ca09e1899689f5712dd79f07646236
julien_grima <gogrinformatique@gmail.com>
2016-09-15 23:23:52 +0200

    MISE A JOUR ET RECTIFICATION DE INDEX
    Changement des tuiles zoom / card reveal
    
    ajout parallax

--- index.php
+++ index.php
@@ -23,14 +23,7 @@ include('includes/header.php');
 </div>
 </div>-->
    
-    <!--<div class="parallax-container">
-        <div class="parallax">
-            <img src="images/secret_garden.jpg">
-            <div><p class="colorbis">TESTTESTESTEFSFEFF?SCKWCJ</p></div>
-            
-        </div>
-    </div>-->
-   
+          <!-- DEBUT ROW1 -->
            <div class="container"> 
                 <div class="row">
                     <div class="col s12 m6 center">
@@ -64,7 +57,19 @@ include('includes/header.php');
                             <source src="videos/present.flv"> <a href="https://www.youtube.com/watch?v=szGkJgvt2-0">Alternative de la vidéo :</a> </video>
                     </div>
                 </div>
-                <!-- Ligne de tuiles lien vers pages 1 -->
+            </div>
+               
+                <!-- FIN ROW1  -->
+                
+                <!-- PARALLAX2 -->
+                <div class="parallax-container">
+                    <div class="parallax">
+                        <img src="images/secret_garden.jpg">
+                    </div>
+                </div>
+                
+                <!-- DEBUT ROW2 TUILES DE LIEN -->
+            <div class="container"> 
                 <div class="row">
                     <div class="col s12 m6 l4">
                         <div class="card">
@@ -72,12 +77,44 @@ include('includes/header.php');
                                 <img class="activator" alt="livraison services" src="images/galets.jpg">
                             </div>
                             <div class="card-content"> 
-                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                                <p><a href="#" class="lien">This is a link</a></p>
+                                <span class="card-title activator grey-text text-darken-4">MINERAL<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">Aller voir</a></p>
                             </div>
                             <div class="card-reveal"> 
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                                <span class="card-title grey-text text-darken-4">MINERAL<i class="material-icons right">close</i></span>
+                                <br>
+                                <br>
+                                <div class="marge">
+                                    <table>
+                                        <tr>
+                                            <li><a href="" class="lien">Ardoises</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Briques</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Décoratifs</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Galets</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Gros volumes</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">OPUS</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Pouzzolane</a></li>
+                                        </tr>
+                                        <tr>
+                                            <lI><a href="" class="lien">Sable</a></lI>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Schiste</a></li>
+                                        </tr>   
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -87,12 +124,38 @@ include('includes/header.php');
                                 <img class="activator" alt="livraison services" src="images/chanvre.jpg">
                             </div>
                             <div class="card-content"> 
-                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                                <p><a href="#" class="lien">This is a link</a></p>
+                                <span class="card-title activator grey-text text-darken-4">VEGETAL<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">Aller voir</a></p>
                             </div>
                             <div class="card-reveal"> 
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                                <span class="card-title grey-text text-darken-4">VEGETAL<i class="material-icons right">close</i></span>
+                                <br>
+                                <br>
+                                <div class="marge">
+                                    <table>
+                                        <tr>
+                                            <li><a href="" class="lien">Chanvre</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Ecorce de pin</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Noyaux de fruit</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Plaquette Peuplier</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Terre de Bruyère</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Terre végétale</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Terreau Ecolabel</a></li>
+                                        </tr>   
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -102,17 +165,51 @@ include('includes/header.php');
                                 <img class="activator" alt="livraison services" src="images/gazon.jpg">
                             </div>
                             <div class="card-content"> 
-                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                                <p><a href="#" class="lien">This is a link</a></p>
+                                <span class="card-title activator grey-text text-darken-4">ACCESSOIRES<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">Aller voir</a></p>
                             </div>
                             <div class="card-reveal"> 
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                                <span class="card-title grey-text text-darken-4">ACCESSOIRES<i class="material-icons right">close</i></span>
+                                <br>
+                                <br>
+                                <div class="marge">
+                                    <table>
+                                        <tr>
+                                            <li><a href="" class="lien">Bordures</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Dalles alvéolées</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Engrais</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Gazon</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Gazon de placage</a></li>
+                                        </tr>
+                                        <tr>
+                                            <li><a href="" class="lien">Traverse</a></li>
+                                        </tr>  
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- Ligne de tuiles lien vers pages 2 -->
+            </div>
+                <!-- FIN ROW2 -->
+                
+                <!-- PARALLAX 2 -->
+                <div class="parallax-container">
+                    <div class="parallax">
+                        <img src="images/copeaubois.jpg">
+                    </div>
+                </div>
+                
+                <!-- DEBUT ROW3 TUILE DE LIEN  -->
+            <div class="container"> 
                 <div class="row">
                     <div class="col s12 m6 l6">
                         <div class="card">
@@ -120,12 +217,36 @@ include('includes/header.php');
                                 <img class="activator" alt="livraison services" src="images/livraison">
                             </div>
                             <div class="card-content"> 
-                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                                <p><a href="#" class="lien">This is a link</a></p>
+                                <span class="card-title activator grey-text text-darken-4">NOS SERVICES<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#" class="lien">Aller voir</a></p>
                             </div>
                             <div class="card-reveal"> 
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                                <span class="card-title grey-text text-darken-4">NOS SERVICES<i class="material-icons right">close</i></span>
+                                <br>
+                                <br>
+                                <div class="marge">
+                                    <table>
+                                        <tr>
+                                           <li><a href="" class="lien">Compte client</a></li>
+                                        </tr>
+                                        <tr>
+                                           <li><a href="" class="lien">Conseils de pro</a></li>
+                                        </tr>
+                                        <tr>
+                                           <li><a href="" class="lien">Devis gratuit</a></li>
+                                        </tr>
+                                        <tr>
+                                           <li><a href="" class="lien">Livraison</a></li>
+                                        </tr>
+                                        <tr>
+                                           <li><a href="" class="lien">Pré-commande</a></li>
+                                        </tr>
+                                        <tr>
+                                           <li><a href="" class="lien">Recherche de produits<br>
+                                                sur commande</a></li>
+                                        </tr>   
+                                    </table>
+                                </div>
                             </div>
                         </div>
 
@@ -136,24 +257,35 @@ include('includes/header.php');
                                 <img class="activator" alt="livraison services" src="images/concept1">
                             </div>
                             <div class="card-content"> 
-                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                                <p><a href="#" class="lien">This is a link</a></p>
+                                <span class="card-title activator grey-text text-darken-4">CONCEPTIONS<i class="material-icons right">more_vert</i></span>
+                                <p><a href="concept.php" class="lien">Aller voir</a></p>
                             </div>
                             <div class="card-reveal"> 
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                                <span class="card-title grey-text text-darken-4">CONCEPTIONS<i class="material-icons right">close</i></span>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <div class="marge">
+                                    <table>
+                                        <tr>
+                                           <li><a href="concept.php" class="lien">Nos conceptions</a></li>
+                                        </tr>   
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-    
+            <!-- FIN DU CONTAINER -->
   
-    <!--<div class="parallax-container">
-        <div class="parallax">
-            <img src="images/secret_garden.jpg">
-        </div>
-    </div>-->
-
+            <div class="parallax-container">
+                <div class="parallax">
+                    <img src="images/herbe.jpg">
+                </div>
+            </div>
 
     <?php include('includes/footer.php'); ?>
